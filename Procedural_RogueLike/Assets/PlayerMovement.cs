95458ce3db354f3b80da4aa31b1a9d528b8f0121
S&K <sdg4631@gmail.com>
2018-06-28 13:39:51 +0900

    Added basic player movement

---
+++ Procedural_RogueLike/Assets/PlayerMovement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlayerMovement : MonoBehaviour 
+{
+	Rigidbody2D myRigidBody;
+
+	[SerializeField] float horizontalMoveSpeed = 5;
+	[SerializeField] float verticalMoveSpeed = 4;
+
+	void Start() 
+	{
+		myRigidBody = GetComponent<Rigidbody2D>();
+	}
+	
+	void Update() 
+	{
+		Move();
+	}
+
+	void Move()
+	{
+		var xThrow = Input.GetAxis("Horizontal");
+		var xVelocity = new Vector2(xThrow * horizontalMoveSpeed, myRigidBody.velocity.y);
+		myRigidBody.velocity = xVelocity;
+		
+		var yThrow = Input.GetAxis("Vertical");
+		var yVelocity = new Vector2(myRigidBody.velocity.x, yThrow * verticalMoveSpeed);
+		myRigidBody.velocity = yVelocity;
+	
+	}
+}
