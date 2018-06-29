5b56c907bad5748f159a9017f68b3f9908652e12
S&K <sdg4631@gmail.com>
2018-06-29 16:03:11 +0900

    Added Half of Pits rigging and animation, colliders for entry room

--- Procedural_RogueLike/Assets/PlayerMovement.cs
+++ Procedural_RogueLike/Assets/PlayerMovement.cs
@@ -5,10 +5,17 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour 
 {
-	Rigidbody2D myRigidBody;
-
+	// Config
 	[SerializeField] float horizontalMoveSpeed = 5;
 	[SerializeField] float verticalMoveSpeed = 4;
+
+	// Cached Component References
+	Rigidbody2D myRigidBody;
+
+	[SerializeField] GameObject pitForward;
+	[SerializeField] GameObject pitForwardLR;
+	
+
 
 	void Start() 
 	{
@@ -17,18 +24,62 @@ public class PlayerMovement : MonoBehaviour
 	
 	void Update() 
 	{
-		Move();
+		Move();		
 	}
 
 	void Move()
+    {
+        // Player Input Returns
+        var xThrow = Input.GetAxis("Horizontal");
+        var yThrow = Input.GetAxis("Vertical");
+
+        // X Movement 
+        var xVelocity = new Vector2(xThrow * horizontalMoveSpeed, myRigidBody.velocity.y);
+        myRigidBody.velocity = xVelocity;
+
+        // Y Movement 
+        var yVelocity = new Vector2(myRigidBody.velocity.x, yThrow * verticalMoveSpeed);
+        myRigidBody.velocity = yVelocity;
+
+        AnimateSprite(xThrow, yThrow);
+		FlipSprite();
+
+    }
+
+    void AnimateSprite(float xThrow, float yThrow)
+    {
+        // Set Animation States
+        bool playerNotMoving = xThrow == 0 && yThrow == 0;
+        bool playerMovingForward = xThrow == 0 && yThrow < 0;
+        bool playerMovingLeft = xThrow < 0 && yThrow <= 0;
+        bool playerMovingRight = xThrow > 0 && yThrow <= 0;
+        bool playerMovingBack = xThrow == 0 && yThrow > 0;
+        bool playerMovingBackLeft = xThrow < 0 && yThrow > 0;
+        bool playerMovingBackRight = xThrow > 0 && yThrow > 0;
+
+        if (playerMovingForward)
+            print("playerMovingForward");
+
+        if (playerMovingLeft)
+            print("playerMovingLeft");
+
+        if (playerMovingRight)
+            print("playerMovingRight");
+
+        if (playerNotMoving)
+            print("playerNotMoving");
+
+        pitForwardLR.SetActive(playerMovingLeft || playerMovingRight);
+        pitForward.SetActive(playerNotMoving || playerMovingForward);
+        pitForward.GetComponent<Animator>().SetBool("RunForward", playerMovingForward);
+    }
+
+    void FlipSprite()
 	{
-		var xThrow = Input.GetAxis("Horizontal");
-		var xVelocity = new Vector2(xThrow * horizontalMoveSpeed, myRigidBody.velocity.y);
-		myRigidBody.velocity = xVelocity;
-		
-		var yThrow = Input.GetAxis("Vertical");
-		var yVelocity = new Vector2(myRigidBody.velocity.x, yThrow * verticalMoveSpeed);
-		myRigidBody.velocity = yVelocity;
-	
+		bool playerHasHorizontalSpeed = Mathf.Abs(myRigidBody.velocity.x) > Mathf.Epsilon;
+		if (playerHasHorizontalSpeed)
+		{
+			pitForwardLR.transform.localScale = new Vector2(-Mathf.Sign(myRigidBody.velocity.x), 1f);
+		}
 	}
 }
