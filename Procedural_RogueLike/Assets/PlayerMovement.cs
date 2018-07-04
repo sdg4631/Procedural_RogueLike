5ac81e63cd70c860193ad7cedaae506b2ea4d5be
S&K <sdg4631@gmail.com>
2018-07-04 11:06:44 +0900

    Aim with Cursor V2
    Full implementation of cursor aiming dictating sprite direction as well as sprite direction according to movement without cursor movement

--- Procedural_RogueLike/Assets/PlayerMovement.cs
+++ Procedural_RogueLike/Assets/PlayerMovement.cs
@@ -17,68 +17,45 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] GameObject pitBack;
     [SerializeField] GameObject pitBackLR;
 
+    // Cursor Variables
+    bool aimingWithCursor = false;
+    float cursorLastMovedTimer;
+    Vector3 lastMouseCoordinate = Vector3.zero;
+    
+
 	void Start() 
 	{
 		myRigidBody = GetComponent<Rigidbody2D>();
 	}
 	
-	void Update() 
-	{
-		Move();		
+	void Update()
+    {
+        CheckForCursorMovement();
+        Move();
+        ControlSpriteWithAiming();
+    }
 
-        Vector3 cursor = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10);
-        cursor = Camera.main.ScreenToWorldPoint(cursor);
-        cursor = cursor - transform.position;
-
-        float angle = Mathf.Atan2(cursor.y - transform.position.y, cursor.x - transform.position.x) * Mathf.Rad2Deg;
-        print(angle);
-
-        if (angle >= -135 && angle <= -45)
-        {
-            pitForward.SetActive(true);
-            pitForwardLR.SetActive(false);
-            pitBack.SetActive(false);
-            pitBackLR.SetActive(false);
-            
-        }
-        else if (((angle < -135 && angle > -180) || (angle < 180 && angle > 145)) || ((angle < 0 && angle > -45) || (angle >= 0 && angle < 35)))
-        {
-            pitForwardLR.SetActive(true);
-            pitForward.SetActive(false);
-            pitBack.SetActive(false);
-            pitBackLR.SetActive(false);
-        }
-        else if (angle <= 95 && angle >= 85)
-        {
-            pitBack.SetActive(true);
-            pitForward.SetActive(false);
-            pitForwardLR.SetActive(false);
-            pitBackLR.SetActive(false);
-        }
-        else if ((angle <= 135 && angle > 95) || (angle < 85 && angle >= 45))
-        {
-            pitBackLR.SetActive(true);
-            pitForward.SetActive(false);
-            pitForwardLR.SetActive(false);
-            pitBack.SetActive(false);
+    void CheckForCursorMovement()
+    {
+        if (Input.mousePosition != lastMouseCoordinate) // Moving Cursor
+        { 
+            cursorLastMovedTimer = 0.0f;
+            aimingWithCursor = true;
         }
 
-        //Pit Facing Left
-        if (((angle < -135 && angle > -180) || (angle < 180 && angle > 145)) || (angle <= 135 && angle > 95))
-        {
-            pitForwardLR.transform.localScale = new Vector2(1, 1f);
-            pitBackLR.transform.localScale = new Vector2(1, 1f);
+        if (Input.mousePosition == lastMouseCoordinate) // Not Moving Cursor
+        { 
+            cursorLastMovedTimer += Time.deltaTime;
         }
 
-        // Pit Facing Right
-        if (((angle < 0 && angle > -45) || (angle >= 0 && angle < 35)) || (angle < 85 && angle >= 45))
+        if (cursorLastMovedTimer >= 1f)
         {
-            pitForwardLR.transform.localScale = new Vector2(-1, 1f);
-            pitBackLR.transform.localScale = new Vector2(-1, 1f);
+            aimingWithCursor = false;
         }
-	}
+        lastMouseCoordinate = Input.mousePosition;
+    }
 
-	void Move()
+    void Move()
     {
         // Player Input Returns
         var xThrow = Input.GetAxis("Horizontal");
@@ -92,15 +69,17 @@ public class PlayerMovement : MonoBehaviour
         var yVelocity = new Vector2(myRigidBody.velocity.x, yThrow * verticalMoveSpeed);
         myRigidBody.velocity = yVelocity;
 
-        // AnimateSprite(xThrow, yThrow);
-		// FlipSprite();
+        if (aimingWithCursor == false)
+        {
+            ControlSpriteWithMoving(xThrow, yThrow);
+		    FlipSpriteWhileMoving();
+        }
+        
 
     }
 
-    void AnimateSprite(float xThrow, float yThrow)
+    void ControlSpriteWithMoving(float xThrow, float yThrow)
     {    
-
-
         bool playerNotMoving = xThrow == 0 && yThrow == 0;
         bool playerMovingForward = xThrow >= -0.25 && xThrow <= 0.25 && yThrow < 0;
         bool playerMovingLeft = xThrow < 0 && yThrow < 0.5;
@@ -159,7 +138,7 @@ public class PlayerMovement : MonoBehaviour
         // }
     }
 
-    void FlipSprite()
+    void FlipSpriteWhileMoving()
 	{
 		bool playerHasHorizontalSpeed = Mathf.Abs(myRigidBody.velocity.x) > Mathf.Epsilon;
 		if (playerHasHorizontalSpeed)
@@ -168,4 +147,77 @@ public class PlayerMovement : MonoBehaviour
             pitBackLR.transform.localScale = new Vector2(-Mathf.Sign(myRigidBody.velocity.x), 1f);
 		}
 	}
+
+    void ControlSpriteWithAiming()
+    {
+        if (aimingWithCursor == true) // TODO or firing???
+        {
+           Vector3 cursor = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10);
+            cursor = Camera.main.ScreenToWorldPoint(cursor);
+            cursor = cursor - transform.position;
+
+            
+
+            float angle = Mathf.Atan2(cursor.y - transform.position.y, cursor.x - transform.position.x) * Mathf.Rad2Deg;
+
+            var facingForward = angle >= -135 && angle <= -45;
+            var facingForwardLR = ((angle < -135 && angle > -180) || (angle < 180 && angle > 145)) || ((angle < 0 && angle > -45) || (angle >= 0 && angle < 35));
+            var facingBack = angle <= 105 && angle >= 75;
+            var facingBackLR = (angle <= 135 && angle > 105) || (angle < 75 && angle >= 45);
+
+            // Activate/Deactivate Parent GameObject
+            if (facingForward)
+            {
+                pitForward.SetActive(true);
+                pitForwardLR.SetActive(false);
+                pitBack.SetActive(false);
+                pitBackLR.SetActive(false);
+
+            }
+            else if (facingForwardLR)
+            {
+                pitForwardLR.SetActive(true);
+                pitForward.SetActive(false);
+                pitBack.SetActive(false);
+                pitBackLR.SetActive(false);
+            }
+            else if (facingBack)
+            {
+                pitBack.SetActive(true);
+                pitForward.SetActive(false);
+                pitForwardLR.SetActive(false);
+                pitBackLR.SetActive(false);
+            }
+            else if (facingBackLR)
+            {
+                pitBackLR.SetActive(true);
+                pitForward.SetActive(false);
+                pitForwardLR.SetActive(false);
+                pitBack.SetActive(false);
+            }
+
+            FlipSpriteWhileAiming(angle);
+        } 
+    }
+        
+    void FlipSpriteWhileAiming(float angle)
+    {
+        var spriteFacingLeft = ((angle < -135 && angle > -180) || (angle < 180 && angle > 145)) || (angle <= 135 && angle > 95);
+        var spriteFacingRight = ((angle < 0 && angle > -45) || (angle >= 0 && angle < 35)) || (angle < 85 && angle >= 45);
+
+        if (spriteFacingLeft)
+        {
+            pitForwardLR.transform.localScale = new Vector2(1, 1f);
+            pitBackLR.transform.localScale = new Vector2(1, 1f);
+        }
+
+
+        if (spriteFacingRight)
+        {
+            pitForwardLR.transform.localScale = new Vector2(-1, 1f);
+            pitBackLR.transform.localScale = new Vector2(-1, 1f);
+        }
+    }
+
+
 }
