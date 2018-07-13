ce6037675de8bdce96b28c33327564c7da9706f1
S&K <sdg4631@gmail.com>
2018-07-13 17:04:15 +0900

    Added PuffShroom animations, script, attack particles
    TODO improve movement to account for walls

---
+++ Procedural_RogueLike/Assets/PuffShroom.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuffShroom : MonoBehaviour 
+{
+	Rigidbody2D myRigidBody;
+	Animator myAnimator;
+	PlayerMovement player;
+
+	[SerializeField] GameObject puffFXPrefab;
+	[SerializeField] GameObject fxParent; 
+	[SerializeField] Transform target;
+	[SerializeField] int timeBetweenPuffsMin = 3;
+	[SerializeField] int timeBetweenPuffsMax = 9;
+
+	private bool isAlive = true;
+	
+
+	void Start() 
+	{
+		myRigidBody = GetComponent<Rigidbody2D>();
+		myAnimator = GetComponent<Animator>();
+		StartCoroutine(StartPuffAnimation());	
+	}
+
+	void Update()
+	{
+
+	}
+
+    void PuffParticles()
+    {
+		myAnimator.ResetTrigger("Puff");
+        var puffFX = Instantiate(puffFXPrefab, transform.position, Quaternion.identity);
+		puffFX.transform.parent = fxParent.transform;
+		float destroyDelay = 4f;
+        Destroy(puffFX, destroyDelay);
+    }
+
+    void Move()
+    {
+		var xVelocity = Random.Range(-5, 5);
+		var yVelocity = Random.Range(-5, 5);
+        myRigidBody.velocity = new Vector2(xVelocity, yVelocity);
+
+		
+		// To be called in Update for continuous chasing movement
+		// transform.position += (player.transform.position - transform.position).normalized * MoveSpeed * Time.deltaTime;
+    }
+
+	void Stop()
+	{
+		myRigidBody.velocity = new Vector2(0,0);
+	}
+
+	IEnumerator StartPuffAnimation()
+	{
+		int timeBetweenPuffs = Random.Range(timeBetweenPuffsMin, timeBetweenPuffsMax);
+
+		while(isAlive)
+		{
+			yield return new WaitForSeconds(timeBetweenPuffs);
+			myAnimator.SetTrigger("Puff");
+		}
+		
+	}
+}
