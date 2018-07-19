2d7955238deabc9ba25da8c7ac4862ad5babb131
S&K <sdg4631@gmail.com>
2018-07-19 10:13:05 +0900

    Added player health, screen shake on hit, and invulnerability duration.

--- Procedural_RogueLike/Assets/Scripts/PlayerHealthAndDamage.cs
+++ Procedural_RogueLike/Assets/Scripts/PlayerHealthAndDamage.cs
@@ -4,16 +4,51 @@ using UnityEngine;
 
 public class PlayerHealthAndDamage : MonoBehaviour 
 {
+	[SerializeField] public Stat health;
+	CameraShake cameraShake;
 
+	[SerializeField] private bool isInvulnerable = false;
+	[SerializeField] private float invulnerabilityTimer = 2f;
+	[SerializeField] private float invulnerabilityDuration = 2f;
+
+	void Awake()
+	{
+		health.Initialize();
+	}
 
 	void Start() 
 	{
-		
+		cameraShake = FindObjectOfType<CameraShake>();
 	}
 	
 
 	void Update() 
 	{
-		
+		InvulnerabilityTimer();
 	}
+
+	void OnCollisionEnter2D(Collision2D other)
+	{
+		if (other.gameObject.tag == "Enemy")
+		{
+			if (!isInvulnerable)
+            {
+                isInvulnerable = true;
+                invulnerabilityTimer = 0f;
+                StartCoroutine(cameraShake.Shake(.2f, .2f, 0));
+                health.CurrentVal--;
+                
+            }
+		}
+	}
+
+    private void InvulnerabilityTimer()
+    {
+        invulnerabilityTimer += Time.deltaTime; 
+
+		 if (invulnerabilityTimer > invulnerabilityDuration)
+		{
+			isInvulnerable = false;
+		}
+    }
 }
---
+++ Procedural_RogueLike/Assets/Scripts/Stat.cs
@@ -0,0 +1,48 @@
+﻿﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System; // // needed to make Serializable without monobehavior attached
+
+[Serializable]
+public class Stat 
+{
+    // [SerializeField] private BarScript bar;
+    [SerializeField] private float maxVal;
+    [SerializeField] private float currentVal;
+
+    public float CurrentVal
+    {
+        get
+        {
+            return currentVal;
+        }
+
+        set
+        {
+            currentVal = Mathf.Clamp(value, 0, MaxVal); // clamp so that health can't exceed maxVal set in inspector
+            // bar.Value = currentVal;
+        }
+    }
+
+    public float MaxVal
+    {
+        get
+        {
+            return maxVal;
+        }
+
+        set
+        {
+            maxVal = value;
+            // bar.MaxValue = maxVal;
+        }
+    }
+
+    // maxVal and currentVal set in the inspector
+    public void Initialize()
+    {
+        this.MaxVal = maxVal;
+        this.CurrentVal = currentVal;
+    }
+}
+
