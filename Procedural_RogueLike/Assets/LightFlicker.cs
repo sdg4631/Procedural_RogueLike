f8d80ebb30fa29126e57b845bc5bbf7f6407c66e
S&K <sdg4631@gmail.com>
2018-08-03 11:44:45 +0900

    Added new rooms from photoshop added fire and torches with light flicker

---
+++ Procedural_RogueLike/Assets/LightFlicker.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightFlicker : MonoBehaviour 
+{
+	Light2D myLight;
+
+	[SerializeField] float minRange = 1.25f;
+	[SerializeField] float maxRange = 1.75f;
+
+	[SerializeField] float minTimeBetweenFlicker = 1f;
+	[SerializeField] float maxTimeBetweenFlicker = 2f;
+	[SerializeField] float flickerTimer = 0f;
+
+	void Start() 
+	{
+		myLight = GetComponent<Light2D>();
+	}
+	
+
+	void Update() 
+	{
+		float random = Random.Range(minRange, maxRange);
+		float timeBetweenFlicker = Random.Range(minTimeBetweenFlicker, maxTimeBetweenFlicker);
+
+		flickerTimer += Time.deltaTime;
+		if (flickerTimer >= timeBetweenFlicker)
+		{
+			myLight.Range = random;
+			flickerTimer = 0f;
+		}
+		
+	}
+}
