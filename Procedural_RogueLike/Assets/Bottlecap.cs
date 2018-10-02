1b909eaffdd2340f000fe6ea3e389e1c859d1b8a
S&K <sdg4631@gmail.com>
2018-10-02 23:21:46 +0900

    Added Bottlecap animations and particle effects

--- Procedural_RogueLike/Assets/Bottlecap.cs
+++ Procedural_RogueLike/Assets/Bottlecap.cs
@@ -4,16 +4,16 @@ using UnityEngine;
 
 public class Bottlecap : MonoBehaviour 
 {
+	[SerializeField] GameObject fizzFX;
 
-
-	void Start() 
+	void OnCollisionEnter2D(Collision2D other)
 	{
-		
-	}
-	
-
-	void Update() 
-	{
-		
+		if (other.gameObject.tag == "Player")
+		{
+			var fizz = Instantiate(fizzFX, transform.position, fizzFX.transform.rotation);
+			gameObject.SetActive(false);
+			Destroy(fizz, 2f);
+			Destroy(gameObject, 2f);
+		}
 	}
 }
