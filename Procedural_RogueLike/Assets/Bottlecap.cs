af0b1f47d6a98efe9c13e0b2831975ba8b29f5cd
S&K <sdg4631@gmail.com>
2018-10-03 10:35:24 +0900

    Added bottlecap pickup SFX

--- Procedural_RogueLike/Assets/Bottlecap.cs
+++ Procedural_RogueLike/Assets/Bottlecap.cs
@@ -5,13 +5,25 @@ using UnityEngine;
 public class Bottlecap : MonoBehaviour 
 {
 	[SerializeField] GameObject fizzFX;
+	[SerializeField] AudioClip pickupSFX;
+
+	AudioSource audioSource;
+	BoxCollider2D mycollider;
+
+	void Start()
+	{
+		audioSource = GetComponent<AudioSource>();
+		mycollider = GetComponent<BoxCollider2D>();
+	}
 
 	void OnCollisionEnter2D(Collision2D other)
 	{
 		if (other.gameObject.tag == "Player")
 		{
+			audioSource.PlayOneShot(pickupSFX);
 			var fizz = Instantiate(fizzFX, transform.position, fizzFX.transform.rotation);
-			gameObject.SetActive(false);
+			mycollider.enabled = false;
+			gameObject.transform.GetChild(0).gameObject.SetActive(false);
 			Destroy(fizz, 2f);
 			Destroy(gameObject, 2f);
 		}
